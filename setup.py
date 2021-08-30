3a989ecf903a6e3def77c4560db1a9292f3caf90
hanjinliu <hanjin.liu@bs.s.u-tokyo.ac.jp>
2021-08-31 01:01:05 +0900

    update setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("mtprops/__init__.py", encoding="utf-8") as f:
+    line = next(f)
+    VERSION = line.strip().split()[-1][1:-1]
+      
+setup(name="MTProps",
+      version=VERSION,
+      author="Hanjin Liu",
+      author_email="liuhanjin-sc@g.ecc.u-tokyo.ac.jp",
+      license="GPLv2",
+      packages=find_packages(),
+      install_requires=[
+            "scikit-image>=0.18",
+            "numpy>=1.17",
+            "scipy>=1.6.3",
+            "matplotlib",
+            "pandas>=1",
+            "dask>=2021.6.0",
+            "napari>=0.4.10",
+      ],
+      python_requires=">=3.7",
+      )
