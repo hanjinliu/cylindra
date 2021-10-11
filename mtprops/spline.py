bug fix and masking, v0.4.9

mtprops/spline.py
@@ -392,15 +392,33 @@ class Spline3D:
                             rise_per_px: float = 0.0,
                             skew_per_px: float = 0.0
                             ) -> np.ndarray:
-        # TODO: test me
-        cyl_coords = self.cylindrical(r_range, s_range)
-        
+        s0, s1 = s_range
+        length = self.length(start=s0, stop=s1)
+        stop_length, n_segs = interval_divmod(length, self.scale)
+        n_pixels = n_segs + 1
+        s2 = (s1 - s0) * stop_length/length + s0
+        if n_pixels < 2:
+            raise ValueError("Too short. Change 's_range'.")
+        u = np.linspace(s0, s2, n_pixels)
+        y_ax_coords = self(u)/self.scale # world coordinates of y-axis in spline coords system
+        dslist = self(u, 1).astype(np.float32)
+        map_ = _polar_coords_2d(*r_range)
+        zeros = np.zeros(map_.shape[:-1], dtype=np.float32)
+        ones = np.ones(map_.shape[:-1], dtype=np.float32)
+        map_slice = np.stack([map_[..., 0], 
+                              zeros,
+                              map_[..., 1],
+                              ones
+                              ], axis=2) # V, S, H, D
+        # TODO: each slice must be rotated separately, with different skew
         mtx = np.array([[1.0,         0.0,         0.0, 0.0],
                         [0.0,         1.0, skew_per_px, 0.0],
                         [0.0, rise_per_px,         1.0, 0.0],
-                        [0.0,         0.0,         0.0, 1.0]])
+                        [0.0,         0.0,         0.0, 1.0]], 
+                       dtype=np.float32)
         
-        return cyl_coords.dot(mtx)
+        map_slice = map_slice @ mtx
+        return _rot_with_vector(map_slice, y_ax_coords, dslist)
     
     def inv_cartesian(self,
                       coords: np.ndarray,
@@ -566,7 +584,7 @@ def _polar_coords_2d(r_start: int, r_stop: int) -> np.ndarray:
     coords[:] = np.flip(coords, axis=0)
     coords[:] = np.flip(coords, axis=1)
     return coords
-
+    
 def _cartesian_coords_2d(lenv, lenh):
     v, h = np.indices((lenv, lenh), dtype=np.float32)
     v -= (lenv/2 - 0.5)

mtprops/tomogram.py
@@ -413,7 +413,7 @@ class MtTomogram:
         """        
         spl = self._paths[i]
         length = spl.length()
-        npoints = max(ceilint(length/max_interval) + 1, 2)
+        npoints = max(ceilint(length/max_interval) + 1, 4)
         interval = length/(npoints-1)
         spl.make_anchors(n=npoints)
         subtomograms = self._sample_subtomograms(i, rotate=False)
@@ -436,10 +436,13 @@ class MtTomogram:
             img.rotate(-angle, cval=np.median(img), update=True)
         
         subtomo_proj = subtomograms.proj("y")
+        shape = subtomo_proj[0].shape
         shifts = np.zeros((npoints, 2)) # zx-shift
+        mask = ip.circular_mask(radius=[s//4 for s in shape], shape=shape)
+        
         for i in range(npoints):
             img = subtomo_proj[i]
-            shifts[i] = ip.pcc_maximum(img[::-1, ::-1], img)/2
+            shifts[i] = ip.pcc_maximum(img[::-1, ::-1], img, mask=mask)/2
         
         # Update spline coordinates.
         coords = spl()
@@ -460,10 +463,10 @@ class MtTomogram:
         return self
     
     @batch_process
-    def refine_fit(self, 
-                   i: int = None,
-                   *, 
-                   max_interval: nm = 30.0) -> MtTomogram:
+    def refine(self, 
+               i: int = None,
+               *, 
+               max_interval: nm = 30.0) -> MtTomogram:
         """
         Refine spline using the result of previous fit and the global structural parameters.
         During refinement, Y-projection of MT XZ cross section is rotated with the skew angle,
@@ -483,7 +486,7 @@ class MtTomogram:
         """        
         spl = self._paths[i]
         length = spl.length()
-        npoints = ceilint(length/max_interval) + 1
+        npoints = max(ceilint(length/max_interval) + 1, 4)
         interval = length/(npoints-1)
         spl.make_anchors(n=npoints)
         subtomograms = self._sample_subtomograms(i)
@@ -508,11 +511,13 @@ class MtTomogram:
         iref = npoints//2
         imgref = imgs_rot[iref]
         shifts = np.zeros((npoints, 2)) # zx-shift
+        shape = imgref.shape
+        mask = ip.circular_mask(radius=[s//4 for s in shape], shape=shape) # mask for PCC
         imgs_aligned = []
         for i in range(npoints):
             img = imgs_rot[i]
             if i != iref:
-                shifts[i] = ip.pcc_maximum(imgref, img)
+                shifts[i] = ip.pcc_maximum(imgref, img, mask=mask)
             else:
                 shifts[i] = np.array([0, 0])
                 
@@ -520,13 +525,13 @@ class MtTomogram:
         
         # Make template
         imgcory = np.stack(imgs_aligned, axis="y").proj("y")
-        center_shift = ip.pcc_maximum(imgcory, imgcory[::-1, ::-1])            
+        center_shift = ip.pcc_maximum(imgcory, imgcory[::-1, ::-1], mask=mask)
         template = imgcory.affine(translation=center_shift/2, mode="reflect")
         
         # Align skew-corrected images to the template
         for i in range(npoints):
             img = imgs_rot[i]
-            shifts[i] = ip.pcc_maximum(template, img)
+            shifts[i] = ip.pcc_maximum(template, img, mask=mask)
 
         coords = spl()
         for i in range(npoints):
@@ -539,7 +544,7 @@ class MtTomogram:
             zxrot = np.array([[cos,  0., sin],
                               [0.,   1.,  0.],
                               [-sin, 0., cos]])
-            
+                        
             mtx = spl.rotation_matrix(spl.anchors[i])[:3, :3]
             coords[i] += shift @ zxrot @ mtx * self.scale
         
@@ -850,9 +855,11 @@ class MtTomogram:
         # align each fragment
         imgs[0] = imgs[0][:, :ylen]
         ref = imgs[0]
+        shape = ref.shape
+        mask = ip.circular_mask(radius=[s//4 for s in shape], shape=shape)
         for i in range(1, len(imgs)):
             img = imgs[i][:, :ylen]
-            shift = ip.pcc_maximum(img, ref)
+            shift = ip.pcc_maximum(img, ref, mask=mask)
             imgs[i] = img.affine(translation=shift, mode="grid-wrap")
 
         out = np.stack(imgs, axis="p").proj("p")
@@ -949,9 +956,11 @@ class MtTomogram:
         # align each fragment
         imgs[0] = imgs[0][f"y=:{ylen}"]
         ref = imgs[0]
+        shape = ref.shape
+        mask = ip.circular_mask(radius=[s//4 for s in shape], shape=shape)
         for i in range(1, len(imgs)):
             img = imgs[i][f"y=:{ylen}"]
-            shift = ip.pcc_maximum(img, ref)
+            shift = ip.pcc_maximum(img, ref, mask=mask)
             imgs[i] = img.affine(translation=shift, 
                                     dims="rya", mode="grid-wrap")
 