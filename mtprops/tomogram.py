refine rotation bug fix

mtprops/tomogram.py
@@ -14,7 +14,7 @@ from .const import nm, H, K, Ori, Mode, CacheKey, GVar
 from .spline import Spline3D
 from .cache import ArrayCacheMap
 from .utils import (load_a_subtomogram, centroid, map_coordinates, roundint, load_rot_subtomograms,
-                    ceilint, oblique_meshgrid, no_verbose, mirror_pcc)
+                    ceilint, oblique_meshgrid, no_verbose, mirror_pcc, mirror_ft_pcc)
 
 cachemap = ArrayCacheMap(maxgb=ip.Const["MAX_GB"])
 LOCALPROPS = [H.splPosition, H.splDistance, H.riseAngle, H.yPitch, H.skewAngle, H.nPF, H.start]
@@ -593,6 +593,7 @@ class MtTomogram:
         npoints = len(spl)
         interval = spl.length()/(npoints-1)
         subtomograms = self._sample_subtomograms(i)
+        subtomograms: ip.ImgArray = subtomograms - subtomograms.mean()
         
         # Calculate Fourier parameters by cylindrical transformation along spline.
         # Skew angles are divided by the angle of single protofilament and the residual
@@ -619,21 +620,17 @@ class MtTomogram:
             inputs = subtomograms.proj("y")["x=::-1"]
         else:
             inputs = subtomograms["x=::-1"]
-        
-        cval = self._background_intensity or np.mean(inputs)
-        
+                
         imgs_rot_list: list[ip.ImgArray] = []
         for i, ang in enumerate(skew_angles):
-            rotimg = inputs[i].rotate(ang, dims="zx", mode=Mode.constant, cval=cval)
+            rotimg = inputs[i].rotate(ang, dims="zx", mode=Mode.constant, cval=0)
             imgs_rot_list.append(rotimg)
-            
+        
         imgs_rot: ip.ImgArray = np.stack(imgs_rot_list, axis="p")
         imgs_rot_ft = imgs_rot.fft(dims=imgs_rot_list[0].axes)
 
         # Coarsely align skew-corrected images
-        iref = npoints//2
-        ft_ref = imgs_rot_ft[iref]
-        shape = ft_ref.shape
+        shape = imgs_rot_ft[0].shape
         # mask for PCC
         mask = ip.circular_mask(radius=[s//4 for s in shape], shape=shape) 
         if not projection and mask_8nm:
@@ -645,15 +642,15 @@ class MtTomogram:
         for i in range(npoints):
             img: ip.ImgArray = imgs_rot[i]
             ft = imgs_rot_ft[i]
-            shift = ip.ft_pcc_maximum(ft_ref, ft, mask=mask)
+            shift = mirror_ft_pcc(ft, mask=mask)/2
             if not projection:
                 shift[1] = 0
-            imgs_aligned.append(img.affine(translation=-shift, mode=Mode.constant, cval=cval))
+            imgs_aligned.append(img.affine(translation=shift, mode=Mode.constant, cval=0))
             
         # Make template using coarse aligned images.
         imgcory: ip.ImgArray = sum(imgs_aligned)
         center_shift = mirror_pcc(imgcory, mask=mask)/2
-        template = imgcory.affine(translation=center_shift, mode=Mode.constant, cval=cval)
+        template = imgcory.affine(translation=center_shift, mode=Mode.constant, cval=0)
         template_ft = template.fft(dims=template.axes)
                 
         # Align skew-corrected images to the template
@@ -665,8 +662,8 @@ class MtTomogram:
                 shift = shift[[0, 2]]
             rad = np.deg2rad(skew_angles[i])
             cos, sin = np.cos(rad), np.sin(rad)
-            zxrot = np.array([[ cos, sin],
-                              [-sin, cos]], dtype=np.float32)
+            zxrot = np.array([[cos,-sin],
+                              [sin, cos]], dtype=np.float32)
             shifts[i] = shift @ zxrot
         
         # Update spline parameters
@@ -1570,7 +1567,7 @@ def dask_angle_corr(imgs, ang_centers, drot: float=7, nrots: int = 29):
     return da.compute(tasks, scheduler=SCHEDULER)[0]
     
 def _local_dft_params(img: ip.ImgArray, radius: nm):
-    img = img - img.mean() # NOTE: rescale for now, but this should be solved in other ways.
+    img = img - img.mean()
     l_circ: nm = 2*np.pi*radius
     npfmin = GVar.nPFmin
     npfmax = GVar.nPFmax

mtprops/utils.py
@@ -76,6 +76,7 @@ def load_a_rot_subtomogram(img: ip.ImgArray, length_px: int, width_px: int, spl:
     out.set_scale(img)
     return out
 
+
 def load_rot_subtomograms(img: ip.ImgArray | ip.LazyImgArray, length_px: int, width_px: int,
                           spl: "Spline3D"):
     plane_shape = (width_px, width_px)
@@ -90,6 +91,7 @@ def load_rot_subtomograms(img: ip.ImgArray | ip.LazyImgArray, length_px: int, wi
     out = ip.asarray(np.stack(out, axis=0), axes="pzyx")
     out.set_scale(img)
     return out
+
 
 def centroid(arr: np.ndarray, xmin: int, xmax: int) -> float:
     """
@@ -111,6 +113,7 @@ def rotational_average(img: ip.ImgArray, fold: int = 13):
     average_img /= fold
     return average_img
 
+
 def interval_divmod(value: float, interval: float) -> tuple[float, int]:
     """
     Calculate stop and n_segs, where satisfy:
@@ -123,6 +126,7 @@ def interval_divmod(value: float, interval: float) -> tuple[float, int]:
     n_segs, res = divmod(value + 1e-8, interval)
     return value - res, int(n_segs)
 
+
 def mirror_pcc(img0: ip.ImgArray, mask=None):
     """
     Phase cross correlation of an image and its mirror image.
@@ -131,7 +135,16 @@ def mirror_pcc(img0: ip.ImgArray, mask=None):
     """    
     ft0 = img0.fft()
     
-    shape = img0.shape
+    return mirror_ft_pcc(ft0, mask)
+
+
+def mirror_ft_pcc(ft0: ip.ImgArray, mask=None):
+    """
+    Phase cross correlation of an image and its mirror image.
+    Identical to ``ip.ft_pcc_maximum(img0, img0[::-1, ::-1])``
+    ``ft0`` must be FFT of ``img0``.
+    """    
+    shape = ft0.shape
     ind = np.indices(shape)
     phase = np.sum([ix/n for ix, n in zip(ind, shape)])
     weight = np.exp(1j*2*np.pi*phase)
@@ -139,6 +152,7 @@ def mirror_pcc(img0: ip.ImgArray, mask=None):
     ft1 = weight*ft0.conj()
     return ip.ft_pcc_maximum(ft0, ft1, mask)
     
+
 def map_coordinates(input: np.ndarray | "da.core.Array", 
                     coordinates: np.ndarray,
                     order: int = 3, 
@@ -171,6 +185,7 @@ def map_coordinates(input: np.ndarray | "da.core.Array",
                                cval=cval,
                                prefilter=order>1
                                )
+
 
 def oblique_meshgrid(shape: tuple[int, int], 
                      rise: float = 0.,