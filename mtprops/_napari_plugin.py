eb7d5223a83a7779a55dab0445fdf1eb6ed40f3c
hanjinliu <hanjin.liu@bs.s.u-tokyo.ac.jp>
2022-01-17 00:15:08 +0900

    prepare for napari plugin

---
+++ mtprops/_napari_plugin.py
@@ -0,0 +1,6 @@
+from napari_plugin_engine import napari_hook_implementation
+from .widget import MTPropsWidget
+
+@napari_hook_implementation
+def napari_experimental_provide_dock_widget():
+    return MTPropsWidget
