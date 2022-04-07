d2259c7e23983f977e9f924eca46badc7f6a1eed
hanjinliu <hanjin.liu@bs.s.u-tokyo.ac.jp>
2022-04-08 00:13:09 +0900

    update required magic-class version

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     install_requires=[
         "impy-array>=1.26.2",
         "magicgui>=0.4.0",
-        "magic-class>=0.6.2",
+        "magic-class>=0.6.3",
         "pyqtgraph>=0.12.4",
         "mrcfile>=1.3.0",
         "napari>=0.4.15",
