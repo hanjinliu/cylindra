63c9c4ae9dff59bc29135e55dd61d7af814c3b51
hanjinliu <hanjin.liu@bs.s.u-tokyo.ac.jp>
2022-04-26 20:46:43 +0900

    update for mac

--- mtprops/__init__.py
+++ mtprops/__init__.py
@@ -2,6 +2,12 @@ __version__ = "0.9.0.dev0"
 __author__ = "Hanjin Liu"
 __email__ = "liuhanjin-sc@g.ecc.u-tokyo.ac.jp"
 
+import sys
+
+if sys.platform == "darwin":
+    from qtpy.QtCore import QCoreApplication, Qt
+    QCoreApplication.setAttribute(Qt.AA_DontUseNativeMenuBar)
+
 from .core import start
 from .components import MtTomogram
 from .widgets import MTPropsWidget
