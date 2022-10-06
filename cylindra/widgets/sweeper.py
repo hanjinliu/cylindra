cb8cfc6807e6a138b707b26edc0244afbc719e4c
hanjinliu <liuhanjin-sc@g.ecc.u-tokyo.ac.jp>
2022-10-06 19:15:16 +0900

    support binsize

--- cylindra/widgets/sweeper.py
+++ cylindra/widgets/sweeper.py
@@ -1,6 +1,6 @@
 from typing import TYPE_CHECKING
-from magicclass import magicclass, field, vfield, MagicTemplate, do_not_record, bind_key, set_design
-from magicclass.types import Bound
+from magicclass import magicclass, field, vfield, MagicTemplate, do_not_record, bind_key
+from magicclass.types import Optional
 from magicclass.ext.pyqtgraph import QtImageCanvas
 import impy as ip
 
@@ -9,12 +9,22 @@ from ..const import GVar, nm
 
 if TYPE_CHECKING:
     from .main import CylindraMainWidget
-    from ..components import CylSpline
 
 @magicclass
 class SplineSweeper(MagicTemplate):
-    show_what = vfield(options={"choices": ["R-projection", "Y-projection", "CFT"]})
+    def _get_available_binsize(self, widget=None) -> "list[int]":
+        from .main import CylindraMainWidget
+        try:
+            parent = self.find_ancestor(CylindraMainWidget)
+            return parent._get_available_binsize(widget)
+        except Exception:
+            return []
+
+    show_what = vfield(options={"choices": ["Y-projection", "R-projection", "CFT"]})
     depth = vfield(32.0, options={"min": 1.0, "max": 200.0})
+    binsize = vfield(options={"choices": _get_available_binsize})
+    radius = vfield(Optional[nm], label="Radius (nm)", options={"text": "Use spline radius", "options": {"max": 100.0}})
+    
     canvas = field(QtImageCanvas, options={"lock_contrast_limits": True})
 
     @property
@@ -29,10 +39,10 @@ class SplineSweeper(MagicTemplate):
         
         Attributes
         ----------
-        spline : CylSpline
-            Current spline object to analyze.
-        pos : int
-            Position along the spline.
+        spline_id : int
+            Current spline ID to analyze.
+        pos : nm
+            Position along the spline in nm.
         """
         def _get_spline_id(self, widget=None) -> "list[tuple[str, int]]":
             from .main import CylindraMainWidget
@@ -42,8 +52,8 @@ class SplineSweeper(MagicTemplate):
             except Exception:
                 return []
             
-        spline_id = vfield(options={"choices": _get_spline_id})
-        pos = field(int, label="Position", options={"max": 0}, record=False)
+        spline_id = vfield(label="Spline", options={"choices": _get_spline_id})
+        pos = field(nm, label="Position from the tip (nm)", widget_type="FloatSlider", options={"max": 0}, record=False)
         
         @bind_key("Up")
         @do_not_record
@@ -71,15 +81,21 @@ class SplineSweeper(MagicTemplate):
     def _spline_changed(self, idx: int):
         try:
             spl = self.parent.tomogram.splines[idx]
-            nanchors = len(spl.anchors)
-            self.controller.pos.max = max(nanchors - 1, 0)
+            self.controller.pos.max = max(spl.length(), 0)
         except Exception:
             pass
     
     @show_what.connect
     @depth.connect
+    @binsize.connect
+    @radius.connect
     @controller.spline_id.connect
     @controller.pos.connect
+    def _on_widget_state_changed(self):
+        if self.visible:
+            self._update_canvas()
+        return None
+
     def _update_canvas(self):
         _type = self.show_what
         idx = self.controller.spline_id
@@ -98,7 +114,7 @@ class SplineSweeper(MagicTemplate):
         else:
             raise RuntimeError
         self.canvas.image = img
-        # self.canvas.text_overlay.update(visible=True, text=f"{num}-{pos}", color="lime")
+        return None
     
     @show_what.connect
     def _update_clims(self):
@@ -125,20 +141,28 @@ class SplineSweeper(MagicTemplate):
         self.canvas.text_overlay.update(visible=True, text=f"{i}-global", color="magenta")
         return None
 
-    def _current_cartesian_img(self, idx: int, pos: int, depth: nm) -> ip.ImgArray:
+    def _current_cartesian_img(self, idx: int, pos: nm, depth: nm) -> ip.ImgArray:
         """Return local Cartesian image at the current position."""
         tomo = self.parent.tomogram
         spl = tomo.splines[idx]
-        length_px = tomo.nm2pixel(depth)
-        width_px = tomo.nm2pixel(2 * spl.radius * GVar.outer)
+        length_px = tomo.nm2pixel(depth, binsize=self.binsize)
+        if radius := self.radius:
+            width_px = tomo.nm2pixel(2 * radius * GVar.outer, binsize=self.binsize)
+        else:
+            if r := spl.radius:
+                width_px = tomo.nm2pixel(2 * r * GVar.outer, binsize=self.binsize)
+            else:
+                return ip.zeros((1, 1, 1), axes="zyx")
         
-        coords = spl.local_cartesian(
+        coords = spl.translate(
+            [-tomo.multiscale_translation(self.binsize)] * 3
+        ).local_cartesian(
             shape=(width_px, width_px), 
             n_pixels=length_px,
-            u=spl.anchors[pos],
-            scale=tomo.scale
+            u=pos / spl.length(),
+            scale=tomo.scale * self.binsize
         )
-        img = tomo.image
+        img = tomo._get_multiscale_or_original(self.binsize)
         out = map_coordinates(img, coords, order=1)
         out = ip.asarray(out, axes="zyx")
         out.set_scale(img)
@@ -148,19 +172,28 @@ class SplineSweeper(MagicTemplate):
     def _current_cylindrical_img(self, idx: int, pos: int, depth: nm):
         """Return cylindric-transformed image at the current position"""
         tomo = self.parent.tomogram
-        ylen = tomo.nm2pixel(depth)
+        ylen = tomo.nm2pixel(depth, binsize=self.binsize)
         spl = tomo.splines[idx]
         
-        rmin = tomo.nm2pixel(spl.radius*GVar.inner)
-        rmax = tomo.nm2pixel(spl.radius*GVar.outer)
+        if radius := self.radius:
+            rmin = tomo.nm2pixel(radius * GVar.inner, binsize=self.binsize)
+            rmax = tomo.nm2pixel(radius * GVar.outer, binsize=self.binsize)
+        else:
+            if r := spl.radius:
+                rmin = tomo.nm2pixel(r * GVar.inner, binsize=self.binsize)
+                rmax = tomo.nm2pixel(r * GVar.outer, binsize=self.binsize)
+            else:
+                return ip.zeros((1, 1, 1), axes="rya")
         
-        coords = spl.local_cylindrical(
+        coords = spl.translate(
+            [-tomo.multiscale_translation(self.binsize)] * 3
+        ).local_cylindrical(
             r_range=(rmin, rmax), 
             n_pixels=ylen, 
-            u=spl.anchors[pos],
-            scale=tomo.scale
+            u=pos / spl.length(),
+            scale=tomo.scale * self.binsize,
         )
-        img = tomo.image
+        img = tomo._get_multiscale_or_original(self.binsize)
         polar = map_coordinates(img, coords, order=1)
         polar = ip.asarray(polar, axes="rya")  # radius, y, angle
         polar.set_scale(r=img.scale.x, y=img.scale.x, a=img.scale.x)
