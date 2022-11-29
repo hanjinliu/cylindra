2c5532bf252c288193574a41cf1e8df6a328b6e8
hanjinliu <liuhanjin-sc@g.ecc.u-tokyo.ac.jp>
2022-11-29 13:06:55 +0900

    implement dataframe list

--- cylindra/_list.py
+++ cylindra/_list.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from typing import TYPE_CHECKING, Any, Callable, Iterator, Mapping, Sequence
+from typing import TYPE_CHECKING, Any, Iterator, Literal, Sequence
 from pathlib import Path
 import numpy as np
 from cylindra.const import IDName, PropertyNames as H
@@ -9,16 +9,36 @@ if TYPE_CHECKING:
 
 COLUMNS = [H.splDistance, H.splPosition, H.riseAngle, H.yPitch, H.skewAngle, H.nPF, H.start]
 
+def nansize(arr: np.ndarray, axis=0):
+    """Return the number of non-NaN elements in the array along a given axis."""
+    return np.sum(~np.isnan(arr), axis=axis)
+
+def nansem(arr: np.ndarray, axis=0):
+    """Return the standard error of the mean of the array along a given axis."""
+    return np.nanstd(arr, axis=axis) / np.sqrt(nansize(arr, axis=axis))
+
 _AGG_FUNCS = {
     "mean": np.nanmean,
     "median": np.nanmedian,
     "min": np.nanmin,
     "max": np.nanmax,
     "std": np.nanstd,
+    "sem": nansem,
+    "var": np.nanvar,
+    "size": nansize,
     "sum": np.nansum,
 }
 
+_INHERIT_DTYPE = {"min", "max", "size", "sum"}
+
 class DataFrameList(Sequence["pd.DataFrame"]):
+    """
+    List of DataFrames for analysis of multiple local properties.
+    
+    >>> dl = DataFrameList.from_csv("path/to/localprops.csv")
+    >>> dl.agg_id("mean")  # aggregate over all splines
+    >>> dl.agg_pos("mean")  # aggregate over all spline positions
+    """
     def __init__(self, df_list: Sequence[pd.DataFrame]):
         self._list = list(df_list)
     
@@ -55,7 +75,7 @@ class DataFrameList(Sequence["pd.DataFrame"]):
     def __repr__(self) -> str:
         if len(self) == 0:
             return f"DataFrameList()"
-        strs = []
+        strs: list[str] = []
         for k, v in enumerate(self._list):
             strs.append(f"{k}: DataFrame of shape={v.shape!r}")
             if k > 12:
@@ -65,17 +85,51 @@ class DataFrameList(Sequence["pd.DataFrame"]):
         joined = ",\n\t".join(strs)
         return f"DataFrameList(\n\t{joined}\n)"
 
-    def _agg_pos(self, fn):
+    def agg_pos(self, fn):
+        """Aggregate over all spline positions."""
         return [df.agg(fn) for df in self._list]
     
-    def _agg_id(self, fn):
+    def agg_id(self, fname: str, align: Literal["top", "bottom", "none"] = "top") -> pd.DataFrame:
+        """
+        Aggregate over all spline.
+        
+        Parameters
+        ----------
+        fname : str
+            Name of the function to use for aggregation. Supported functions are: "mean", "median", 
+            "min", "max", "std", "sem", "var", "size" and "sum".
+        align : {"top", "bottom", "none"}, optional
+            How to align the data frames before aggregation if lengths are different. If "top", they
+            will be aligned at the index 0. If "bottom", they will be aligned at the last index. If
+            "none", exception will be raised if lengths are different.
+        """
         import pandas as pd
+        
+        fn = _AGG_FUNCS[fname]
         max_len = max(len(df) for df in self._list)
         all_df = []
         for df in self._list:
             nr, nc = df.shape
             if nr < max_len:
-                df = pd.concat([df, np.full((max_len - nr, nc), np.nan)])
+                if align == "none":
+                    raise ValueError("DataFrames must be of equal length.")
+                
+                nan_arr = np.full((max_len - nr, nc), np.nan)
+                if align == "top":
+                    df = pd.concat(
+                        [df, pd.DataFrame(nan_arr, columns=df.columns)],
+                        axis=0,
+                        ignore_index=True,
+                    )
+                elif align == "bottom":
+                    df = pd.concat(
+                        [pd.DataFrame(nan_arr, columns=df.columns), df],
+                        axis=0,
+                        ignore_index=True,
+                    )
+                else:
+                    raise ValueError(f"Invalid align value: {align!r}")
+
             all_df.append(df)
 
             columns = df.columns
@@ -84,7 +138,12 @@ class DataFrameList(Sequence["pd.DataFrame"]):
         for col in columns:
             stacked = np.stack([df[col] for df in all_df], axis=1)
             df_out_dict[col] = fn(stacked, axis=1)
-        return pd.DataFrame(df_out_dict)
+        
+        df_out = pd.DataFrame(df_out_dict)
+        if fname in _INHERIT_DTYPE:
+            for col in columns:
+                df_out[col] = df_out[col].astype(self._list[0][col].dtype)
+        return df_out
             
     def collect(self) -> pd.DataFrame:
         """Collect all the child data frames into a single data frame."""
@@ -104,29 +163,24 @@ class DataFrameList(Sequence["pd.DataFrame"]):
     
     def select(self, col):
         cls = type(self)
-        return cls({k: df[col] for k, df in self.items()})
-
-    def agg_id(self, fname: str):
-        fn = _AGG_FUNCS[fname]
-        return self._agg_id(fn)
-    
-    def agg_pos(self, fn):
-        return self._agg_pos(fn)      
+        return cls([df[col] for df in self])
 
 
 class LocIndexer:
-    def __init__(self, dd: DataFrameList):
-        self._dd = dd
+    """Vectorized loc indexer."""
+    def __init__(self, dfl: DataFrameList):
+        self._dfl = dfl
     
     def __getitem__(self, key) -> pd.DataFrame:
-        cls = type(self._dd)
-        return cls([df.loc[key] for df in self._dd])
+        cls = type(self._dfl)
+        return cls([df.loc[key] for df in self._dfl])
     
 
 class ILocIndexer:
-    def __init__(self, dd: DataFrameList):
-        self._dd = dd
+    """Vectorized iloc indexer."""
+    def __init__(self, dfl: DataFrameList):
+        self._dfl = dfl
     
     def __getitem__(self, key) -> pd.DataFrame:
-        cls = type(self._dd)
-        return cls([df.iloc[key] for df in self._dd])
+        cls = type(self._dfl)
+        return cls([df.iloc[key] for df in self._dfl])
---
+++ tests/test_dataframe_list.py
@@ -0,0 +1,38 @@
+from typing import Sequence
+from cylindra._list import DataFrameList
+import pandas as pd
+from pandas.testing import assert_frame_equal
+
+def _assert_eq(a: Sequence[pd.Series], b: Sequence[pd.Series]):
+    assert len(a) == len(b)
+    for a0, b0 in zip(a, b):
+        assert a0.equals(b0)
+    
+def test_agg_pos():
+    dfl = DataFrameList([
+        pd.DataFrame({"a": [1, 2, 3], "b": [4, 5, 6]}),
+        pd.DataFrame({"a": [5, 4, 3], "b": [2, 1, 2]}),
+        pd.DataFrame({"a": [1, 1, 1], "b": [1, 1, 1]}),
+    ])
+    
+    _sum = dfl.agg_pos("sum")
+    _assert_eq(_sum, [pd.Series({"a": 6, "b": 15}), pd.Series({"a": 12, "b": 5}), pd.Series({"a": 3, "b": 3})])
+    
+    _min = dfl.agg_pos("min")
+    _assert_eq(_min, [pd.Series({"a": 1, "b": 4}), pd.Series({"a": 3, "b": 1}), pd.Series({"a": 1, "b": 1})])
+    
+    _max = dfl.agg_pos("max")
+    _assert_eq(_max, [pd.Series({"a": 3, "b": 6}), pd.Series({"a": 5, "b": 2}), pd.Series({"a": 1, "b": 1})])
+
+def test_agg_id():
+    dfl = DataFrameList([
+        pd.DataFrame({"a": [1, 2, 3], "b": [4, 5, 6]}),
+        pd.DataFrame({"a": [5, 4], "b": [2, 1]}),
+        pd.DataFrame({"a": [1, 1, 1], "b": [1, 1, 1]}),
+    ])
+    
+    assert_frame_equal(dfl.agg_id("sum", align="top"), pd.DataFrame({"a": [7, 7, 4], "b": [7, 7, 7]}))
+    assert_frame_equal(dfl.agg_id("size", align="top"), pd.DataFrame({"a": [3, 3, 2], "b": [3, 3, 2]}))
+    
+    assert_frame_equal(dfl.agg_id("sum", align="bottom"), pd.DataFrame({"a": [2, 8, 8], "b": [5, 8, 8]}))
+    assert_frame_equal(dfl.agg_id("size", align="bottom"), pd.DataFrame({"a": [2, 3, 3], "b": [2, 3, 3]}))
