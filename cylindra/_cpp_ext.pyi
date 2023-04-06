bd49b3dfd1ced0ff04faa233531fda59381d88fa
Hanjin Liu <hanjin.liu@bs.s.u-tokyo.ac.jp>
2023-04-06 21:31:03 +0900

    use class

--- cylindra/_cpp_ext.pyi
+++ cylindra/_cpp_ext.pyi
@@ -1,28 +1,20 @@
 from __future__ import annotations
 import numpy as np
 
-def viterbi(
-    score: np.ndarray,
-    origin: np.ndarray,
-    zvec: np.ndarray,
-    yvec: np.ndarray,
-    xvec: np.ndarray,
-    dist_min: float,
-    dist_max: float,
-) -> tuple[np.ndarray, float]:
-    ...
 
-def viterbiAngularConstraint(
-    score: np.ndarray,
-    origin: np.ndarray,
-    zvec: np.ndarray,
-    yvec: np.ndarray,
-    xvec: np.ndarray,
-    dist_min: float,
-    dist_max: float,
-    skew_max: float,
-) -> tuple[np.ndarray, float]:
-    ...
+class ViterbiGrid:
+    def __init__(
+        self,
+        score_array: np.ndarray,
+        origin: np.ndarray,
+        zvec: np.ndarray,
+        yvec: np.ndarray,
+        xvec: np.ndarray,
+    ) -> None:
+        ...
+    
+    def viterbi_simple(self, dist_min: float, dist_max: float) -> tuple[np.ndarray, float]: ...
+    def viterbi(self, dist_min: float, dist_max: float, skew_max: float) -> tuple[np.ndarray, float]: ...
 
 def alleviate(
     arr: np.ndarray,
