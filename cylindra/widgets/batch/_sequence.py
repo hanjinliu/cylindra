b8547b14e6d88d21759793ecb70e035245c526bb
Hanjin Liu <hanjin.liu@bs.s.u-tokyo.ac.jp>
2023-04-19 11:32:17 +0900

    install pre-commit

--- cylindra/widgets/batch/_sequence.py
+++ cylindra/widgets/batch/_sequence.py
@@ -1,8 +1,16 @@
 from typing import Iterator
 from fnmatch import fnmatch
-from magicgui.widgets import ComboBox, Container
+from magicgui.widgets import ComboBox, Container, Widget
 from magicclass import (
-    do_not_record, magicclass, field, magicmenu, nogui, vfield, MagicTemplate, set_design, abstractapi
+    do_not_record,
+    magicclass,
+    field,
+    magicmenu,
+    nogui,
+    vfield,
+    MagicTemplate,
+    set_design,
+    abstractapi,
 )
 from magicclass.types import Path, ExprStr
 from magicclass.widgets import Separator
@@ -17,15 +25,22 @@ from cylindra.const import GlobalVariables as GVar, nm, MoleculesHeader as Mole,
 from cylindra.widgets.widget_utils import FileFilter
 from ._localprops import LocalPropsViewer
 
-@magicclass(labels=False, properties={"margins": (0, 0, 0, 0)}, record=False, layout="horizontal")
+
+@magicclass(
+    labels=False,
+    properties={"margins": (0, 0, 0, 0)},
+    record=False,
+    layout="horizontal",
+)
 class MoleculeWidget(MagicTemplate):
     check = vfield(True).with_options(text="")
     line = field("").with_options(enabled=False)
-    
+
     def _get_molecules(self) -> Molecules:
         fp = Path(self.line.value)
         mole = Molecules.from_csv(self.line.value)
         return mole.with_features([pl.repeat(fp.stem, pl.count()).alias(Mole.id)])
+
 
 @magicclass(widget_type="collapsible", record=False, name="Molecules")
 class MoleculeList(MagicTemplate):
@@ -38,10 +53,17 @@ class MoleculeList(MagicTemplate):
         wdt["check"].text = ""
         self.append(wdt)
 
-@magicclass(labels=False, record=False, properties={"margins": (0, 0, 0, 0)}, layout="horizontal")
+
+@magicclass(
+    labels=False,
+    record=False,
+    properties={"margins": (0, 0, 0, 0)},
+    layout="horizontal",
+)
 class SplineWidget(MagicTemplate):
     check = vfield(True).with_options(text="")
     line = field("").with_options(enabled=False)
+
 
 @magicclass(widget_type="collapsible", record=False, name="Splines")
 class SplineList(MagicTemplate):
@@ -55,10 +77,20 @@ class SplineList(MagicTemplate):
         self.append(wdt)
 
 
-@magicclass(widget_type="frame", labels=False, record=False, properties={"margins": (0, 0, 0, 0)})
+@magicclass(
+    widget_type="frame",
+    labels=False,
+    record=False,
+    properties={"margins": (0, 0, 0, 0)},
+)
 class Project(MagicTemplate):
     # a widget representing a single project
-    @magicclass(layout="horizontal", labels=False, properties={"margins": (0, 0, 0, 0)}, record=False)
+    @magicclass(
+        layout="horizontal",
+        labels=False,
+        properties={"margins": (0, 0, 0, 0)},
+        record=False,
+    )
     class Header(MagicTemplate):
         """
         Project info.
@@ -70,16 +102,17 @@ class Project(MagicTemplate):
         path : str
             Project path.
         """
+
         check = vfield(True).with_options(text="")
         path = field("").with_options(enabled=False)
-        
+
         remove_project = abstractapi()
-        
+
         @set_design(text="Open")
         def send_to_viewer(self):
             """Send this project to the viewer."""
             from cylindra.core import instance
-            
+
             if ui := instance():
                 ui.load_project(self.path.value)
             else:
@@ -87,14 +120,14 @@ class Project(MagicTemplate):
 
         def __post_init__(self):
             self["check"].text = ""  # NOTE: should be updated here!
-    
+
     def __init__(self, project: "CylindraProject | None" = None):
         self._project = project
-    
+
     @property
     def project(self) -> "CylindraProject | None":
         return self._project
-    
+
     @Header.wraps
     @set_design(text="✕", max_width=30)
     def remove_project(self):
@@ -107,11 +140,16 @@ class Project(MagicTemplate):
     def _on_checked(self, value: bool):
         self.splines.enabled = value
         self.molecules.enabled = value
-    
-    @magicclass(widget_type="collapsible", name="Components", record=False, properties={"margins": (12, 0, 0, 0)})
+
+    @magicclass(
+        widget_type="collapsible",
+        name="Components",
+        record=False,
+        properties={"margins": (12, 0, 0, 0)},
+    )
     class Components(MagicTemplate):
         pass
-    
+
     splines = Components.field(SplineList)
     molecules = Components.field(MoleculeList)
 
@@ -123,18 +161,18 @@ class Project(MagicTemplate):
         self = cls(project)
         self.Header.path.value = path
         self.Header.path.tooltip = path
-        
+
         # load splines
         for spline_path in project.splines:
             self.splines._add_path(spline_path)
-        
+
         # load molecules
         for mole_path in project.molecules:
             self.molecules._add_path(mole_path)
 
         self.margins = (0, 0, 0, 0)
         return self
-    
+
     @nogui
     def get_loader(self, order: int = 3) -> SubtomogramLoader:
         path = self.Header.path.value
@@ -146,28 +184,28 @@ class Project(MagicTemplate):
             order=order,
             scale=project.scale,
         )
-    
+
     def _get_loader_paths(self) -> tuple[Path, list[Path]]:
         path = self.Header.path.value
         project = CylindraProject.from_json(path)
         img_path = Path(project.image)
         mole_paths = [Path(mole.line.value) for mole in self.molecules if mole.check]
         return img_path, mole_paths
-    
+
     def _get_localprops(self) -> pl.DataFrame:
         path = self.Header.path.value
         project = CylindraProject.from_json(path)
         if project.localprops is None:
             raise ValueError("No localprops file found.")
-        
+
         df = pl.read_csv(project.localprops)
         checked = [spl.check for spl in self.splines]
         return df.filter(pl.col(IDName.spline).is_in(checked))
 
 
 @magicclass(
-    widget_type="scrollable", 
-    labels=False, 
+    widget_type="scrollable",
+    labels=False,
     record=False,
     properties={"min_height": 20, "min_width": 250},
 )
@@ -176,7 +214,7 @@ class ProjectPaths(MagicTemplate):
         prj = Project._from_path(path)
         self.append(prj)
         return prj
-    
+
     def __iter__(self) -> Iterator[Project]:
         return super().__iter__()
 
@@ -184,7 +222,7 @@ class ProjectPaths(MagicTemplate):
     def paths(self) -> list[Path]:
         return [Path(wdt.Header.path.value) for wdt in self]
 
-    
+
 @magicclass(name="Projects", record=False)
 class ProjectSequenceEdit(MagicTemplate):
     """
@@ -202,13 +240,13 @@ class ProjectSequenceEdit(MagicTemplate):
         sep0 = field(Separator)
         load_batch_project = abstractapi()
         save_batch_project = abstractapi()
-    
+
     @magicmenu
     class Select(MagicTemplate):
         select_all_projects = abstractapi()
         select_projects_by_pattern = abstractapi()
         select_molecules_by_pattern = abstractapi()
-    
+
     @magicmenu
     class View(MagicTemplate):
         view_components = abstractapi()
@@ -224,23 +262,27 @@ class ProjectSequenceEdit(MagicTemplate):
 
     seq_name = vfield("Loader").with_options(label="Name:")
     projects = field(ProjectPaths)
-    scale = field(1.0, label="Scale (nm):").with_options(min=0.001, step=0.0001, max=10.0)
+    scale = field(1.0, label="Scale (nm):").with_options(
+        min=0.001, step=0.0001, max=10.0
+    )
     filter_expression = field(ExprStr.In[{"pl": pl}], label="Filter:")
-    
+
     @Select.wraps
     @do_not_record
     def select_all_projects(self):
         """Select all projects."""
         for wdt in self.projects:
             wdt.Header.check = True
-    
+        return None
+
     @Select.wraps
     @do_not_record
     def select_projects_by_pattern(self, pattern: str):
         """Select projects by pattern matching."""
         for prj in self.projects:
             prj.Header.check = fnmatch(prj.Header.path.value, pattern)
-    
+        return None
+
     @Select.wraps
     @do_not_record
     def select_molecules_by_pattern(self, pattern: str):
@@ -248,19 +290,22 @@ class ProjectSequenceEdit(MagicTemplate):
         for prj in self.projects:
             for mole in prj.molecules:
                 mole.check = fnmatch(mole.line.value, pattern)
-    
+        return None
+
     def _get_project_paths(self, _=None) -> list[Path]:
         return [wdt.Header.path.value for wdt in self.projects]
-    
+
     def _get_selected_project_paths(self, _=None) -> list[Path]:
         return [prj.Header.path.value for prj in self._iter_selected_projects()]
-    
+
     def _iter_selected_projects(self) -> Iterator[Project]:
         for prj in self.projects:
             if prj.Header.check:
                 yield prj
-    
-    def _get_batch_loader(self, order: int = 3, output_shape=None, predicate=None) -> BatchLoader:
+
+    def _get_batch_loader(
+        self, order: int = 3, output_shape=None, predicate=None
+    ) -> BatchLoader:
         batch_loader = BatchLoader()
         for i, prj in enumerate(iter(self.projects)):
             if not prj.Header.check:
@@ -273,16 +318,18 @@ class ProjectSequenceEdit(MagicTemplate):
         if output_shape is not None:
             batch_loader = batch_loader.replace(output_shape=output_shape)
         return batch_loader
-    
+
     def _get_loader_paths(self, _=None) -> list[tuple[Path, list[Path]]]:
         return [prj._get_loader_paths() for prj in self.projects]
-    
+
     def _get_localprops(self) -> pl.DataFrame:
         dataframes: list[pl.DataFrame] = []
         for idx, prj in enumerate(iter(self.projects)):
             df = prj._get_localprops()
             dataframes.append(
-                df.with_columns(pl.repeat(idx, pl.count()).cast(pl.UInt16).alias(Mole.image))
+                df.with_columns(
+                    pl.repeat(idx, pl.count()).cast(pl.UInt16).alias(Mole.image)
+                )
             )
         return pl.concat(dataframes, how="diagonal")
 
@@ -291,10 +338,11 @@ class ProjectSequenceEdit(MagicTemplate):
     @set_design(text="View components in 3D")
     def view_components(self):
         """View all the splines and molecules that exist in this project."""
-        from cylindra.project._widgets import ComponentsViewer
+        from cylindra.project import ComponentsViewer
+
         cbox = ComboBox(choices=self._get_project_paths)
         comp_viewer = ComponentsViewer()
-        
+
         self.changed.connect(lambda: cbox.reset_choices())
         cbox.changed.connect(
             lambda path: comp_viewer._from_project(CylindraProject.from_json(path))
@@ -305,14 +353,15 @@ class ProjectSequenceEdit(MagicTemplate):
         cbox.changed.emit(cbox.value)
         return None
 
-    
     @View.wraps
     @set_design(text="View selected components in 3D")
     def view_selected_components(self):
+        """View selected components in a 3D viewer."""
         from cylindra.project import ComponentsViewer
+
         cbox = ComboBox(choices=self._get_selected_project_paths)
         comp_viewer = ComponentsViewer()
-        
+
         self.changed.connect(lambda: cbox.reset_choices())
         cbox.changed.connect(
             lambda path: comp_viewer._from_project(CylindraProject.from_json(path))
@@ -322,12 +371,12 @@ class ProjectSequenceEdit(MagicTemplate):
         cont.show()
         cbox.changed.emit(cbox.value)
         return None
-    
-    
+
     @View.wraps
     @do_not_record
     @set_design(text="View selected molecules in table")
     def view_molecules(self):
+        """View selected molecules in a table"""
         mole = self._get_batch_loader().molecules
         df = mole.to_dataframe()
         if df.shape[0] == 0:
@@ -335,12 +384,13 @@ class ProjectSequenceEdit(MagicTemplate):
         table = DataFrameView(value=df)
         _set_parent(table, self)
         table.show()
+        return None
 
     @View.wraps
     @do_not_record
     @set_design(text="View filtered molecules in table")
     def view_filtered_molecules(self):
-        """Preview filtered molecules."""
+        """Preview filtered molecules in a table."""
         mole = self._get_batch_loader(predicate=self._get_expression()).molecules
         df = mole.to_dataframe()
         if df.shape[0] == 0:
@@ -348,6 +398,7 @@ class ProjectSequenceEdit(MagicTemplate):
         table = DataFrameView(value=df)
         _set_parent(table, self)
         table.show()
+        return None
 
     @View.wraps
     @do_not_record
@@ -358,8 +409,8 @@ class ProjectSequenceEdit(MagicTemplate):
         _set_parent(wdt, self)
         wdt.show()
         wdt._set_localprops(self._get_localprops())
-        return
-        
+        return None
+
     def _get_expression(self, _=None) -> pl.Expr:
         wdt = self.filter_expression
         if wdt.value == "":
@@ -375,8 +426,8 @@ class ProjectSequenceEdit(MagicTemplate):
             wdt = self.projects._add(path)
             self.scale.value = wdt.project.scale
         self.reset_choices()
-        return 
-    
+        return None
+
     @File.wraps
     @do_not_record
     @set_design(text="Add projects with wildcard path")
@@ -389,13 +440,17 @@ class ProjectSequenceEdit(MagicTemplate):
             wdt = self.projects._add(path)
             self.scale.value = wdt.project.scale
         self.reset_choices()
-        return 
-    
+        return
+
     @File.wraps
+    @do_not_record
     def clear_children(self):
+        """Clear all the projects in the list."""
         self.projects.clear()
+        return None
 
     construct_loader = abstractapi()
 
-def _set_parent(wdt, parent):
+
+def _set_parent(wdt: Widget, parent: Widget):
     wdt.native.setParent(parent.native, wdt.native.windowFlags())
