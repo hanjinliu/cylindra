9b0c95353223c7e2d7c163b1b621191afa5b7bc3
Hanjin Liu <hanjin.liu@bs.s.u-tokyo.ac.jp>
2023-06-02 00:32:13 +0900

    fix test

--- tests/test_gui_0.py
+++ tests/test_gui_0.py
@@ -361,7 +361,7 @@ def test_preview(ui: CylindraMainWidget):
 
 def test_slicer(ui: CylindraMainWidget):
     ui.load_project(PROJECT_DIR_14PF, filter=False)
-    ui.open_slicer()
+    ui.ImageMenu.open_slicer()
     ui.spline_slicer.refresh_widget_state()
     ui.spline_slicer.show_what = "CFT"
     ui.spline_slicer.show_what = "R-projection"
@@ -787,15 +787,15 @@ def test_showing_widgets(ui: CylindraMainWidget):
     ui.show_macro()
     ui.show_full_macro()
     ui.show_native_macro()
-    ui.open_logger()
-    ui.open_image_loader()
-    ui.view_project(PROJECT_DIR_13PF / "project.json")
+    ui.Others.open_logger()
+    ui.File.open_image_loader()
+    ui.File.view_project(PROJECT_DIR_13PF / "project.json")
 
 
 def test_spline_clipper(ui: CylindraMainWidget):
     ui.load_project(PROJECT_DIR_13PF, filter=False)
     len_old = ui.get_spline(0).length()
-    ui.open_spline_clipper()
+    ui.Splines.open_spline_clipper()
     ui.spline_clipper.clip_length = 1
     ui.spline_clipper.clip_here()
     assert ui.get_spline(0).length() == pytest.approx(len_old - 1, abs=0.01)
@@ -807,6 +807,6 @@ def test_spline_clipper(ui: CylindraMainWidget):
 
 def test_spectra_measurer(ui: CylindraMainWidget):
     ui.load_project(PROJECT_DIR_13PF, filter=False)
-    ui.open_spectra_measurer()
+    ui.Analysis.open_spectra_measurer()
     ui.spectra_measurer.log_scale = True
     ui.spectra_measurer.log_scale = False
