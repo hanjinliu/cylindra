fix offset sign

cylindra/widgets/sta.py
@@ -35,7 +35,13 @@ import napari
 
 from cylindra import utils, _config
 from cylindra.types import MoleculesLayer, get_monomer_layers
-from cylindra.const import ALN_SUFFIX, MoleculesHeader as Mole, nm, PropertyNames as H
+from cylindra.const import (
+    ALN_SUFFIX,
+    MoleculesHeader as Mole,
+    nm,
+    PropertyNames as H,
+    Ori,
+)
 from cylindra.widgets._widget_ext import (
     CheckBoxes,
     RotationEdit,
@@ -632,11 +638,14 @@ class SubtomogramAveraging(MagicTemplate):
             # write offsets to spline globalprops if available
             if spl := layer.source_spline:
                 if spl.radius is None:
-                    _radius = utils.with_radius(mole, spl)[Mole.radius].mean()
+                    _radius: nm = utils.with_radius(mole, spl)[Mole.radius].mean()
                 else:
                     _radius = spl.radius
                 _offset_y = svec[1]
                 _offset_a = np.arctan2(svec[2], svec[0] + _radius)
+                if spl.orientation is Ori.MinusToPlus:
+                    _offset_y = -_offset_y
+                    _offset_a = -_offset_a
                 spl.globalprops = spl.globalprops.with_columns(
                     pl.Series(H.offset_axial, [_offset_y], dtype=pl.Float32),
                     pl.Series(H.offset_angular, [_offset_a], dtype=pl.Float32),

cylindra/widgets/subwidgets/menus.py
@@ -21,7 +21,7 @@ from magicclass import (
     abstractapi,
 )
 from magicclass.widgets import Separator, ConsoleTextEdit, CodeEdit
-from magicclass.types import Path
+from magicclass.types import Path, Color
 from magicclass.logging import getLogger
 from magicclass.ext.polars import DataFrameView
 
@@ -31,7 +31,7 @@ from cylindra._custom_layers import MoleculesLayer
 from cylindra.utils import roundint
 from cylindra.types import get_monomer_layers, ColoredLayer
 from cylindra.ext.etomo import PEET
-from cylindra.const import nm, get_versions
+from cylindra.const import nm, get_versions, GlobalVariables as GVar
 from cylindra.project import CylindraProject
 from cylindra import _config
 from .global_variables import GlobalVariablesMenu
@@ -239,7 +239,7 @@ class Splines(ChildWidget):
 
 
 @magicmenu(name="Molecules")
-class MoleculesMenu(MagicTemplate):
+class MoleculesMenu(ChildWidget):
     """Operations on molecules"""
 
     @magicmenu
@@ -251,7 +251,50 @@ class MoleculesMenu(MagicTemplate):
         map_centers = abstractapi()
         map_along_pf = abstractapi()
 
-    show_orientation = abstractapi()
+    @set_design(text="Show orientation")
+    @do_not_record
+    def show_orientation(
+        self,
+        layer: MoleculesLayer,
+        x_color: Color = "orange",
+        y_color: Color = "cyan",
+        z_color: Color = "crimson",
+    ):
+        """
+        Show molecule orientations with a vectors layer.
+
+        Parameters
+        ----------
+        {layer}
+        x_color : Color, default is "crimson"
+            Vector color of the x direction.
+        y_color : Color, default is "cyan"
+            Vector color of the y direction.
+        z_color : Color, default is "orange"
+            Vector color of the z direction.
+        """
+        main = self._get_main()
+        mol = layer.molecules
+        nmol = len(mol)
+        name = f"Axes of {layer.name}"
+
+        zvec = np.stack([mol.pos, mol.z], axis=1)
+        yvec = np.stack([mol.pos, mol.y], axis=1)
+        xvec = np.stack([mol.pos, mol.x], axis=1)
+
+        vector_data = np.concatenate([zvec, yvec, xvec], axis=0)
+
+        # TODO: edge color not considered
+        layer = main.parent_viewer.add_vectors(
+            vector_data,
+            edge_width=0.3,
+            edge_color=[z_color] * nmol + [y_color] * nmol + [x_color] * nmol,
+            features={"direction": ["z"] * nmol + ["y"] * nmol + ["x"] * nmol},
+            length=GVar.point_size * 0.8,
+            name=name,
+        )
+        return main._undo_callback_for_layer(layer)
+
     set_source_spline = abstractapi()
     translate_molecules = abstractapi()
 