c02708f24e17380b19a50217cb8f859860043a21
hanjinliu <liuhanjin-sc@g.ecc.u-tokyo.ac.jp>
2023-08-08 19:04:41 +0900

    minor bug fix

--- cylindra/widgets/sta.py
+++ cylindra/widgets/sta.py
@@ -687,7 +687,7 @@ class SubtomogramAveraging(MagicTemplate):
             loader = self._get_loader(bin_size, mole, order=1)
             _img_trans, result = model.fit(
                 loader.average(template.shape),
-                max_shifts=tuple(max_shifts / _scale),
+                max_shifts=[_s / _scale for _s in max_shifts],
             )
 
             rotator = Rotation.from_quat(result.quat)
@@ -1699,7 +1699,7 @@ def _default_align_averaged_shifts(mole: Molecules) -> np.ndarray:
     npf = mole.features[Mole.pf].max() + 1
     dy = np.sqrt(np.sum((mole.pos[0] - mole.pos[1]) ** 2))  # axial shift
     dx = np.sqrt(np.sum((mole.pos[0] - mole.pos[npf]) ** 2))  # lateral shift
-    return np.array([dy, dy, dx]) * 0.6
+    return (dy, dy * 0.6, dx * 0.6)
 
 
 def _update_mole_pos(new: Molecules, old: Molecules, spl: "CylSpline") -> Molecules:
