384c1499d39c6f3add42c3d06c9e057f7a37ad52
Hanjin Liu <liuhanjin-sc@i.softbank.jp>
2025-10-02 21:09:53 +0100

    fix layer controls

--- cylindra/_napari/_layer_controls.py
+++ cylindra/_napari/_layer_controls.py
@@ -8,15 +8,31 @@ from typing import TYPE_CHECKING
 import napari
 import numpy as np
 from magicclass.ext.polars import DataFrameView
-from napari._qt.layer_controls.qt_points_controls import QtPointsControls
+from napari._qt.layer_controls.qt_layer_controls_base import QtLayerControls
 from napari._qt.layer_controls.qt_surface_controls import QtSurfaceControls
-from napari._qt.layer_controls.qt_vectors_controls import QtVectorsControls
+from napari._qt.layer_controls.widgets import (
+    QtOutSliceCheckBoxControl,
+    QtProjectionModeControl,
+    QtTextVisibilityControl,
+    QtWidgetControlsBase,
+)
+from napari._qt.layer_controls.widgets._vectors import (
+    QtEdgeColorFeatureControl,
+    QtVectorStyleComboBoxControl,
+    QtWidthSpinBoxControl,
+)
+from napari._qt.layer_controls.widgets.qt_widget_controls_base import QtWrappedLabel
+from napari._qt.widgets.qt_color_swatch import QColorSwatchEdit
+from napari.layers.base._base_constants import Mode as BaseMode
+from napari.layers.points._points_constants import Mode as PointsMode
+from napari.utils.events import disconnect_events
 from qtpy import QtCore
 from qtpy import QtWidgets as QtW
 from qtpy.QtCore import Qt
 from superqt import QEnumComboBox, QLabeledDoubleSlider
 
 if TYPE_CHECKING:
+    import pandas as pd
     from qtpy.QtWidgets import QFormLayout
 
     from cylindra._napari._layers import (
@@ -42,122 +58,231 @@ class ViewDimension(Enum):
     sphere = 3
 
 
-class QtMoleculesControls(QtPointsControls):
-    layer: MoleculesLayer
+class QtFaceControls(QtWidgetControlsBase):
+    _layer: MoleculesLayer
 
-    def __init__(self, layer: MoleculesLayer) -> None:
-        super().__init__(layer)
-        # hide buttons to avoid mutable operations
-        self.panzoom_button.hide()
-        self.select_button.hide()
-        self.addition_button.hide()
-        self.delete_button.hide()
-        self.transform_button.hide()
+    def __init__(
+        self,
+        parent: QtW.QWidget,
+        layer: MoleculesLayer,
+        tooltip: str | None = None,
+    ) -> None:
+        super().__init__(parent, layer)
+        # Setup layer
+        self._layer.events.face_color.connect(self._on_face_color_change)
 
-        self.faceColorEdit.color_changed.disconnect()
-        self.faceColorEdit.color_changed.connect(self._change_face_color)
-        self.borderColorEdit.color_changed.disconnect()
-        self.borderColorEdit.setColor(_first_or(layer.border_color, "dimgray"))
-        self.borderColorEdit.color_changed.connect(self._change_border_color)
+        # Setup widgets
+        self.face_color_edit = QColorSwatchEdit(
+            initial_color=self._layer.current_face_color,
+            tooltip=tooltip,
+        )
+        self.face_color_label = QtWrappedLabel("face color:")
+        self._on_face_color_change()
+        self.face_color_edit.color_changed.connect(self._on_face_color_edit_changed)
 
-        slider = QLabeledDoubleSlider(orientation=Qt.Orientation.Horizontal)
-        slider.setRange(0.1, 12.0)
-        slider.setSingleStep(0.02)
-        slider.setValue(layer.point_size)
-        slider.setToolTip("Point size of all the molecules")
-        slider.valueChanged.connect(self._change_point_size)
-        self.pointSizeSlider = slider
+    def _on_face_color_change(self) -> None:
+        """Receive layer model face color change event and update color swatch."""
+        with qt_signals_blocked(self.face_color_edit):
+            if isinstance(col := self._layer._colormap_info, str):
+                self.face_color_edit.setColor(col)
+            else:
+                pass  # TODO: set to a gradient
 
-        self.borderWidth = QtW.QDoubleSpinBox()
-        self.borderWidth.setRange(0.0, 1.0)
-        self.borderWidth.valueChanged.connect(self._change_border_width)
-        self.borderWidth.setSingleStep(0.05)
-        self.borderWidth.setValue(_first_or(layer.border_width, 0.05))
-        self.borderWidth.setToolTip("Width of the molecule borders")
+    def _on_face_color_edit_changed(self, color) -> None:
+        self._layer.face_color = color
+
+    def get_widget_controls(self) -> list[tuple[QtWrappedLabel, QtW.QWidget]]:
+        return [(self.face_color_label, self.face_color_edit)]
+
+
+class QtBorderControls(QtWidgetControlsBase):
+    _layer: MoleculesLayer
+
+    def __init__(self, parent: QtW.QWidget, layer: MoleculesLayer) -> None:
+        super().__init__(parent, layer)
+        # Setup layer
+        self.border_color_edit = QColorSwatchEdit(
+            initial_color=self._layer.current_border_color,
+            tooltip="Click to set the border color of currently selected points and any added afterwards.",
+        )
+        self.border_color_edit.color_changed.connect(self._change_border_color)
+        layer.events.border_color.connect(self._on_border_color_change)
+
+        self.border_width_edit = QtW.QDoubleSpinBox()
+        self.border_width_edit.setRange(0.0, 1.0)
+        self.border_width_edit.valueChanged.connect(self._change_border_width)
+        self.border_width_edit.setSingleStep(0.05)
+        self.border_width_edit.setValue(_first_or(layer.border_width, 0.05))
+        self.border_width_edit.setToolTip("Width of the molecule borders")
         layer.events.border_width.connect(self._on_border_width_change)
 
+        self.border_color_edit_label = QtWrappedLabel("border color:")
+        self.border_width_label = QtWrappedLabel("border width:")
+
+    def _on_border_color_change(self, event) -> None:
+        """Receive layer.current_border_color() change event and update view."""
+        with qt_signals_blocked(self.border_color_edit):
+            self.border_color_edit.setColor(
+                _first_or(self._layer.border_color, "dimgray")
+            )
+
+    def _on_border_width_change(self, event):
+        with qt_signals_blocked(self.border_width_edit):
+            self.border_width_edit.setValue(_first_or(self._layer.border_width, 0.05))
+
+    def _change_border_width(self, value: float):
+        self._layer.border_width = value
+
+    def _change_border_color(self, value):
+        self._layer.border_color = value
+
+    def get_widget_controls(self) -> list[tuple[QtWrappedLabel, QtW.QWidget]]:
+        return [
+            (self.border_color_edit_label, self.border_color_edit),
+            (self.border_width_label, self.border_width_edit),
+        ]
+
+
+class QtPointStateControl(QtWidgetControlsBase):
+    _layer: MoleculesLayer
+
+    def __init__(self, parent: QtW.QWidget, layer: MoleculesLayer) -> None:
+        super().__init__(parent, layer)
+        # Point size slider
+        self.point_size_slider = QLabeledDoubleSlider(
+            orientation=Qt.Orientation.Horizontal
+        )
+        self.point_size_slider.setRange(0.1, 12.0)
+        self.point_size_slider.setSingleStep(0.02)
+        self.point_size_slider.setValue(layer.point_size)
+        self.point_size_slider.setToolTip("Point size of all the molecules")
+        self.point_size_slider.valueChanged.connect(self._change_point_size)
+        layer.events.point_size.connect(self._on_point_size_change)
+
+        # View dimension combo box
         self.dimComboBox = QEnumComboBox(enum_class=ViewDimension)
         self.dimComboBox.currentEnumChanged.connect(self._change_dim)
         self.dimComboBox.setCurrentEnum(ViewDimension(layer.view_ndim))
+        layer.events.view_ndim.connect(self._on_dim_change)
 
-        layout: QFormLayout = self.layout()
-        layout.addRow("point size:", self.pointSizeSlider)
-        layout.addRow("border width:", self.borderWidth)
-        layout.addRow("view mode:", self.dimComboBox)
-
-        btns = QtW.QHBoxLayout()
-        self.showFeatureButton = QtW.QPushButton("show", self)
-        self.showFeatureButton.setToolTip("Show features of the molecules in a table")
-        self.showFeatureButton.clicked.connect(self._show_features)
-        btns.addWidget(self.showFeatureButton)
-        self.copyFeatureButton = QtW.QPushButton("copy", self)
-        self.copyFeatureButton.setToolTip("Copy features of the molecules to clipboard")
-        self.copyFeatureButton.clicked.connect(self._copy_features)
-        btns.addWidget(self.copyFeatureButton)
-        layout.addRow("features:", btns)
-
-        self.propertyFilter = QtW.QLineEdit()
-        self.propertyFilter.setPlaceholderText("Filter status tip ...")
-        self.propertyFilter.setToolTip(
+        self.property_filter = QtW.QLineEdit()
+        self.property_filter.setPlaceholderText("Filter status tip ...")
+        self.property_filter.setToolTip(
             "Filter the mouse-hover status tip by the column name.\n"
             "e.g. 'align-* will match all columns starting with 'align-'\n"
         )
-        self.propertyFilter.editingFinished.connect(self._set_property_filter)
-        layout.addRow("filter status:", self.propertyFilter)
+        self.property_filter.editingFinished.connect(self._set_property_filter)
 
-        layout.removeRow(self.sizeSlider)
-        layout.removeRow(self.symbolComboBox)
-        layer.events.point_size.connect(self._on_point_size_change)
-        layer.events.view_ndim.connect(self._on_dim_change)
+        self.point_size_label = QtWrappedLabel("point size:")
+        self.dim_label = QtWrappedLabel("view mode:")
+        self.property_filter_label = QtWrappedLabel("filter status:")
 
     def _on_point_size_change(self, event):
-        with qt_signals_blocked(self.pointSizeSlider):
-            self.pointSizeSlider.setValue(event.value)
+        with qt_signals_blocked(self.point_size_slider):
+            self.point_size_slider.setValue(event.value)
 
     def _on_dim_change(self, event):
         with qt_signals_blocked(self.dimComboBox):
             self.dimComboBox.setCurrentEnum(ViewDimension(event.value))
 
-    def _on_border_width_change(self, event):
-        with qt_signals_blocked(self.borderWidth):
-            self.borderWidth.setValue(_first_or(self.layer.border_width, 0.05))
-
     def _change_point_size(self, value: float):
-        self.layer.point_size = value
-
-    def _change_border_width(self, value: float):
-        self.layer.border_width = value
+        self._layer.point_size = value
 
     def _change_dim(self, value: ViewDimension):
-        self.layer.view_ndim = value.value
+        self._layer.view_ndim = value.value
 
-    def _change_face_color(self, value):
-        self.layer.face_color = value
+    def _set_property_filter(self):
+        text = self.property_filter.text()
+        if text:
+            self._layer._property_filter = lambda x: fnmatch(x, text)
+        else:
+            self._layer._property_filter = lambda _: True
 
-    def _change_border_color(self, value):
-        self.layer.border_color = value
+    def get_widget_controls(self) -> list[tuple[QtWrappedLabel, QtW.QWidget]]:
+        return [
+            (self.point_size_label, self.point_size_slider),
+            (self.dim_label, self.dimComboBox),
+            (self.property_filter_label, self.property_filter),
+        ]
 
-    def _on_current_face_color_change(self):
-        pass
+
+class QtHasFeaturesControls(QtWidgetControlsBase):
+    _layer: MoleculesLayer | InteractionVector
+
+    def __init__(self, parent: QtW.QWidget, layer: MoleculesLayer) -> None:
+        super().__init__(parent, layer)
+        self.feature_btns = QtW.QWidget()
+        btns_layout = QtW.QHBoxLayout(self.feature_btns)
+        btns_layout.setContentsMargins(0, 0, 0, 0)
+        btns_layout.setSpacing(1)
+        self.show_feature_btn = QtW.QPushButton("show", parent)
+        self.show_feature_btn.setToolTip("Show features of the molecules in a table")
+        self.show_feature_btn.clicked.connect(self._show_features)
+        btns_layout.addWidget(self.show_feature_btn)
+        self.copy_feature_btn = QtW.QPushButton("copy", parent)
+        self.copy_feature_btn.setToolTip("Copy features of the molecules to clipboard")
+        self.copy_feature_btn.clicked.connect(self._copy_features)
+        btns_layout.addWidget(self.copy_feature_btn)
+
+        self.feature_buttons_label = QtWrappedLabel("features:")
 
     def _show_features(self):
-        df = self.layer.molecules.features
+        if isinstance(self._layer, MoleculesLayer):
+            df = self._layer.molecules.features
+        else:
+            df = self._layer.net.features
         table = DataFrameView(value=df)
 
         napari.current_viewer().window.add_dock_widget(
-            table, area="left", name=f"Features of {self.layer.name!r}"
+            table, area="left", name=f"Features of {self._layer.name!r}"
         ).setFloating(True)
 
     def _copy_features(self):
-        df = self.layer.features
+        df: pd.DataFrame = self._layer.features
         df.to_clipboard(index=False)
 
-    def _set_property_filter(self):
-        text = self.propertyFilter.text()
-        if text:
-            self.layer._property_filter = lambda x: fnmatch(x, text)
-        else:
-            self.layer._property_filter = lambda _: True
+    def get_widget_controls(self) -> list[tuple[QtWrappedLabel, QtW.QWidget]]:
+        return [
+            (self.feature_buttons_label, self.feature_btns),
+        ]
+
+
+class QtMoleculesControls(QtLayerControls):
+    layer: MoleculesLayer
+    MODE = PointsMode
+    PAN_ZOOM_ACTION_NAME = "activate_points_pan_zoom_mode"
+    TRANSFORM_ACTION_NAME = "activate_points_transform_mode"
+
+    def __init__(self, layer) -> None:
+        super().__init__(layer)
+        self.panzoom_button.hide()
+        self.transform_button.hide()
+
+        self._on_editable_or_visible_change()
+
+        # Setup widgets controls
+        self._projection_mode_control = QtProjectionModeControl(self, layer)
+        self._add_widget_controls(self._projection_mode_control)
+        self._face_color_control = QtFaceControls(
+            self,
+            layer,
+        )
+        self._add_widget_controls(self._face_color_control)
+        self._border_color_control = QtBorderControls(self, layer)
+        self._add_widget_controls(self._border_color_control)
+        self._text_visibility_control = QtTextVisibilityControl(self, layer)
+        self._add_widget_controls(self._text_visibility_control)
+        self._out_slice_checkbox_control = QtOutSliceCheckBoxControl(self, layer)
+        self._add_widget_controls(self._out_slice_checkbox_control)
+        self._point_state_control = QtPointStateControl(self, layer)
+        self._add_widget_controls(self._point_state_control)
+        self._has_features_control = QtHasFeaturesControls(self, layer)
+        self._add_widget_controls(self._has_features_control)
+
+    def close(self):
+        """Disconnect events when widget is closing."""
+        disconnect_events(self.layer.text.events, self)
+        super().close()
 
 
 class QtLandscapeSurfaceControls(QtSurfaceControls):
@@ -165,6 +290,8 @@ class QtLandscapeSurfaceControls(QtSurfaceControls):
 
     def __init__(self, layer: LandscapeSurface) -> None:
         super().__init__(layer)
+        self.panzoom_button.hide()
+        self.transform_button.hide()
         layout: QFormLayout = self.layout()
         self.levelSlider = QLabeledDoubleSlider(orientation=Qt.Orientation.Horizontal)
         self.levelSlider.setRange(layer._level_min, layer._level_max)
@@ -231,46 +358,37 @@ class QtLandscapeSurfaceControls(QtSurfaceControls):
         self.layer.wireframe.width = value
 
 
-class QtInteractionControls(QtVectorsControls):
+class QtInteractionControls(QtLayerControls):
     layer: InteractionVector
+    MODE = BaseMode
+    PAN_ZOOM_ACTION_NAME = "activate_tracks_pan_zoom_mode"
+    TRANSFORM_ACTION_NAME = "activate_tracks_transform_mode"
 
     def __init__(self, layer: InteractionVector) -> None:
         super().__init__(layer)
-        layout: QFormLayout = self.layout()
+        self.panzoom_button.hide()
+        self.transform_button.hide()
+        # Setup widgets controls
+        self._width_spinbox_control = QtWidthSpinBoxControl(self, layer)
+        self._add_widget_controls(self._width_spinbox_control)
+        self._projection_mode_control = QtProjectionModeControl(self, layer)
+        self._add_widget_controls(self._projection_mode_control)
+        self._vector_style_combobox_control = QtVectorStyleComboBoxControl(self, layer)
+        self._add_widget_controls(self._vector_style_combobox_control)
+        self._edge_color_feature_control = QtEdgeColorFeatureControl(self, layer)
+        self._add_widget_controls(self._edge_color_feature_control)
+        self._out_slice_checkbox_control = QtOutSliceCheckBoxControl(self, layer)
+        self._add_widget_controls(self._out_slice_checkbox_control)
 
-        btns = QtW.QHBoxLayout()
-        self.showFeatureButton = QtW.QPushButton("show", self)
-        self.showFeatureButton.setToolTip("Show features of the interaction in a table")
-        self.showFeatureButton.clicked.connect(self._show_features)
-        btns.addWidget(self.showFeatureButton)
-        self.copyFeatureButton = QtW.QPushButton("copy", self)
-        self.copyFeatureButton.setToolTip(
-            "Copy features of the interaction to clipboard"
-        )
-        self.copyFeatureButton.clicked.connect(self._copy_features)
-        btns.addWidget(self.copyFeatureButton)
-        layout.addRow("features:", btns)
+        self._has_features_control = QtHasFeaturesControls(self, layer)
+        self._add_widget_controls(self._has_features_control)
 
-        layout.removeRow(self.lengthSpinBox)
-        layout.removeRow(self.color_mode_comboBox)
         self.autoContrastBtn = QtW.QPushButton("Auto Contrast", self)
-        layout.addWidget(self.autoContrastBtn)
+        self.layout().addWidget(self.autoContrastBtn)
         self.autoContrastBtn.clicked.connect(self._auto_contrast_edge)
 
-    def _show_features(self):
-        df = self.layer.net.features
-        table = DataFrameView(value=df)
-
-        napari.current_viewer().window.add_dock_widget(
-            table, area="left", name=f"Features of {self.layer.name!r}"
-        ).setFloating(True)
-
-    def _copy_features(self):
-        df = self.layer.features
-        df.to_clipboard(index=False)
-
     def _auto_contrast_edge(self):
-        pname = self.color_prop_box.currentText()
+        pname = self._edge_color_feature_control.color_feature_box.currentText()
         values = self.layer.features[pname]
         self.layer.edge_contrast_limits = values.min(), values.max()
         self.layer.refresh()
